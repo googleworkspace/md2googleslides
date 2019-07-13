05d22b5006f1b51cb9f5d9a7792d68b4781669d3
Jiayu Liu <jiayu.liu@airbnb.com>
2019-07-13 11:34:41 +0800

    run ncu
     argparse                                   ^1.0.7  →   ^1.0.10
     extend                                     ^3.0.0  →    ^3.0.2
     file-url                                   ^2.0.2  →    ^3.0.0
     google-auth-library                        ^2.0.2  →    ^4.2.5
     googleapis                                ^36.0.0  →   ^41.0.0
     highlight.js                              ^9.15.6  →   ^9.15.8
     lodash                                   ^4.17.11  →  ^4.17.14
     lowlight                                  ^1.12.0  →   ^1.12.1
     markdown-it                                ^8.0.0  →    ^9.0.1
     markdown-it-attrs                          ^2.3.2  →    ^2.4.1
     markdown-it-emoji                          ^1.2.0  →    ^1.4.0
     markdown-it-expand-tabs                   ^1.0.11  →   ^1.0.13
     mime-types                                ^2.1.22  →   ^2.1.24
     node-fetch                                 ^2.3.0  →    ^2.6.0
     opener                                     ^1.4.2  →    ^1.5.1
     probe-image-size                           ^4.0.0  →    ^4.1.1
     sharp                                     ^0.22.0  →   ^0.22.1
     tmp                                        0.0.33  →     0.1.0
     tmp-promise                                ^1.0.5  →    ^2.0.2
     @babel/cli                                 ^7.2.3  →    ^7.5.0
     @babel/core                                ^7.4.0  →    ^7.5.4
     @babel/plugin-proposal-class-properties    ^7.4.0  →    ^7.5.0
     @babel/preset-env                          ^7.4.2  →    ^7.5.4
     @babel/register                            ^7.4.0  →    ^7.4.4
     @types/lowdb                               ^1.0.7  →    ^1.0.9
     @types/markdown-it                          0.0.7  →     0.0.8
     @types/sharp                              ^0.22.1  →   ^0.22.2
     @typescript-eslint/eslint-plugin           ^1.6.0  →   ^1.12.0
     @typescript-eslint/parser                  ^1.6.0  →   ^1.12.0
     chai-subset                                ^1.3.0  →    ^1.6.0
     eslint                                    ^5.16.0  →    ^6.0.1
     eslint-config-prettier                     ^4.1.0  →    ^6.0.0
     eslint-plugin-prettier                     ^3.0.1  →    ^3.1.0
     mocha                                      ^5.2.0  →    ^6.1.4
     mock-fs                                    ^4.8.0  →   ^4.10.1
     prettier                                  ^1.16.4  →   ^1.18.2
     typescript                                 ^3.4.1  →    ^3.5.3
    
    Run npm install to install new versions.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
-module.exports =  {
-    semi:  true,
-    trailingComma:  'all',
-    singleQuote:  true,
-    printWidth:  120,
-    tabWidth:  4,
-  };
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+};
